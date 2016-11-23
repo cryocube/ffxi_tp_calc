9054b7c1339dc27d85ed6a5c3929c1d5774b879e
Kyle Ricks <kyle@nihil.hsd1.wa.comcast.net>
2016-11-22 23:25:39 -0800

    Some plumbing to prove core equation has been put together.  Next is argparse for variables.

--- calc.py
+++ calc.py
@@ -16,7 +16,7 @@
 #########################################################
 #
 #
-# Command-line Options, Arguments, and Sub-Commands
+import math
 import argparse
 #
 #
@@ -31,7 +31,11 @@ weapon_delay = int()
 ja_stp_lvl = int()
 merit_stp_lvl = int()
 gear_haste = int()
-equation = int()
+delay = float()
+d_mult = float()
+base = float()
+floor = float()
+tp_rate = float()
 #
 # Tuples
 stp_ja = [(1,10), (2,15), (3,20), (4,25), (5,30)]
@@ -42,7 +46,60 @@ stp_ja = [(1,10), (2,15), (3,20), (4,25), (5,30)]
 #                                                       #
 #########################################################
 #
-
+def delay_calc(weapon_delay):
+    if weapon_delay <= 450:
+        equation = int(weapon_delay) - 180
+        return equation
+    elif weapon_delay <= 480:
+        equation = int(weapon_delay) - 450
+        return equation
+    elif weapon_delay <= 530:
+        equation = int(weapon_delay) - 480
+        return equation
+    else:
+        equation = int(weapon_delay) - 530
+        return equation
+#
+#
+def delay_multiplier(weapon_delay):
+    if weapon_delay <= 180:
+        return float(1.5)
+    elif weapon_delay <= 450:
+        return float(6.5)
+    elif weapon_delay <= 530:
+        return float(1.5)    
+    else:
+        return float(3.5)
+#
+#
+def base_sel(weapon_delay):
+    if weapon_delay <= 180:
+        return 180
+    elif weapon_delay <= 450:
+        return 270
+    elif weapon_delay <= 480:
+        return 30
+    elif weapon_delay <= 530:
+        return 50
+    else:
+        return 470
+#
+#
+def floor_sel(weapon_delay):
+    if weapon_delay <= 450:
+        return float(5)
+    elif weapon_delay <= 480:
+        return float(11.5)
+    elif weapon_delay <= 530:
+        return float(13)
+    else:
+        return float(14.5)
+#
+#
+def core_calc(base,d_mult,delay,floor):
+    tp_per_hit = float((floor+((delay*d_mult)/base)))
+    return = math.floor(float(tp_per_hit))
+#
 #
 #########################################################
 #                                                       #
@@ -51,3 +108,14 @@ stp_ja = [(1,10), (2,15), (3,20), (4,25), (5,30)]
 #########################################################
 #
 #
+base = base_sel(weapon_delay)
+delay = delay_calc(weapon_delay)
+d_mult = delay_multiplier(weapon_delay)
+floor = floor_sel(weapon_delay)
+tp_rate = core_calc(base,d_mult,delay,floor)
+print("Base = "+str(base))
+print("Delay = "+str(delay))
+print("Delay Multiplier = "+str(d_mult))
+print("Floor = "+str(floor))
+print("TP per swing is "+str(tp_rate))
+
